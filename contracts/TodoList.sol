3a5c7a3a4eccbdd0b3799507eaa76bb0935a0f9d
unknown <bruneltesse@gmail.com>
2022-04-28 23:12:25 +0100

    loading

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,21 @@
 pragma solidity ^0.5.0;
  
 contract TodoList{
+    constructor( ) public{
+        createTask("Checkout out dappuniversity.com");
+    }
     uint public taskCount = 0;
+    mapping(uint =>Task) public tasks;
+
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
 }
