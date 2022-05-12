bba58892f14ae5b228e5959e7b5f19dff9ceeb61
unknown <bruneltesse@gmail.com>
2022-05-12 10:48:46 +0100

    ERC TOken ok

--- test/AfroToken.js
+++ test/AfroToken.js
@@ -33,4 +33,25 @@ contract('AfroToken', function(accounts) {
         const balanceReceiver = await afroToken.balanceOf(accounts[1]);
         assert.equal(balanceReceiver, 25000, 'Token transfered!')
     })
+
+    it("approved token for delegated transfer", async function() {
+        const afroToken = await AfroToken.deployed();
+        const approved = await afroToken.approve.call(accounts[1], 100);
+        assert.equal(approved, true, 'it returns true');
+    })
+
+    it('Handles delegated token transfers', async function() {
+        const afroToken = await AfroToken.deployed();
+        const fromAccount = accounts[2];
+        const toAccount = accounts[3];
+        const spendingAccount = accounts[4];
+        // Transfer some tokens
+        const receipt1 = await afroToken.transfer(fromAccount, 100, { from: accounts[0] });
+        const receipt2 = await afroToken.approve(spendingAccount, 10, { from: fromAccount });
+        // Try transferring something larger than the sender's balance
+        const receipt3 = await afroToken.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+        receipt3.catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'Cannot transfer value larger than balance');
+        });
+    });
 });
